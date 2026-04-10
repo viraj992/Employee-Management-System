2e92d4bbe114c03998260a53e824b762741e6b8a
viraj992 <it21183836@my.sliit.lk>
2026-04-10 19:40:53 +0530

    Emloyee Dashboard Ui completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react"
+import {dummyEmployeeDashboardData} from "../assets/assets"
+import Loading from "../components/Loading"
+import EmployeeDashboard from "../components/EmployeeDashboard"
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=> {
+    setData(dummyEmployeeDashboardData)
+    setTimeout(()=>{
+      setLoading(false)
+    },1000)
+  },[])
+
+  if (loading) return <Loading/>
+  if (!data) return <p className="text-center text-slate-500 py-12">Failed to load dashboard</p>
+
+  if(data.role === "ADMIN"){
+    return <div>admin dashboard</div>
+  }else{
+    return <EmployeeDashboard data={data}/> // data - propname , {data} - variable name
+  }
+
+  
 }
